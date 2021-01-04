509aa70a59b4b97bde7e210cb23c91bea6fdbd09
ythosa <vasus714@yandex.ru>
2021-01-04 21:39:58 +0300

    Configuration

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/Inexpediency/todo-rest-api/pkg/handler"
 	"github.com/Inexpediency/todo-rest-api/pkg/repository"
 	"github.com/Inexpediency/todo-rest-api/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occured while initializing config: %s", err)
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running the server: %s ", err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Inexpediency/todo-rest-api
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/spf13/viper v1.7.1 // indirect
+)
