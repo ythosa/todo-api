f592f9253ebfb06dcdf3cf027c74f1463e25fab7
ythosa <vasus714@yandex.ru>
2021-01-04 17:24:34 +0300

    Added service and repository

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/Inexpediency/todo-rest-api"
 	"github.com/Inexpediency/todo-rest-api/pkg/handler"
+	"github.com/Inexpediency/todo-rest-api/pkg/repository"
+	"github.com/Inexpediency/todo-rest-api/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
---
+++ pkg/service/service.go
@@ -0,0 +1,25 @@
+package service
+
+import "github.com/Inexpediency/todo-rest-api/pkg/repository"
+
+type Authorization interface {
+
+}
+
+type TodoList interface {
+
+}
+
+type TodoItem interface {
+
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(repos *repository.Repository) *Service {
+	return &Service{}
+}
